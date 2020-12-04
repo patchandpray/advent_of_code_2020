03bd5ed1cc22fdd2b9d0333ddcc7affa39c83233
tim.vandekeer <t.van.de.keer@gmail.com>
2020-12-04 08:51:45 +0100

    day 2, part 1

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,48 +1,64 @@
 use std::env;
 use std::fs;
 
+use regex::Regex;
+
+#[derive(Debug)]
 pub struct PasswordItem {
-    policy: Policy,
+    min: usize,
+    max: usize,
+    c: char,
     password: String,
 }
 
 impl PasswordItem {
     pub fn new(input: &str) -> Self {
-        todo!()
+        // deconstruct input to policy and password
+        let re = Regex::new(r"^(\d+)-(\d+)\s(\w):\s(\w+)$").expect("regex failed");
+        let captures = re.captures(input).unwrap();
+
+        let min = captures.get(1).unwrap().as_str().parse().unwrap();
+        let max = captures.get(2).unwrap().as_str().parse().unwrap();
+        let c = captures.get(3).unwrap().as_str().chars().next().unwrap();
+        let password = captures.get(4).unwrap().as_str().to_string();
+
+
+        PasswordItem{min, max, c, password}
+    }
+
+    pub fn is_valid(&self) -> bool {
+        // check if char occurs min
+        // check if char occurs max
+
+        let count = &self.password.matches(self.c).count();
+        !(count < &self.min || count > &self.max)
+
     }
 }
 
 
-pub struct Policy {
-    char_occurence: [i64; 2],
-    char: char,
-}
-
 fn main() {
-
     // accept input file
-    // iterate over inputs to
-    // ...
-    // get policy & get password from input
-    // deconstruct policy
-    // store policy<password> objects
-    // then
-    // ...
-    // 
-    // apply policy on password
-    // returns True if valid, False if invalid
-    // ...
-    // count occurances of passwords adhering to policy
-    // then
-    // ...
-    // go on holiday to tropical island 🌴
-
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
+    // iterate over inputs to
+    // get policy & get password from input
+    // deconstruct policy
+    // and
+    // store policy<password> objects
     let content = fs::read_to_string(filename).unwrap();
     let password_items: Vec<PasswordItem> = content.lines().map(|line| PasswordItem::new(line)).collect();
 
+    // then
+    // ...
+    // apply policy on password
+    // count occurances of passwords adhering to policy
+    let valid_passwords: usize = password_items.iter().filter(|item| item.is_valid()).count();
 
-    println!("Hello, world!");
+    println!("valid passwords: {}", valid_passwords);
+
+    // ...
+    // go on holiday to tropical island 🌴
+
 }
